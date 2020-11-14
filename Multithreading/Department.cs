6af806215d35f0ecff71193f6025151df68ecf78
Elnur <57497680+ElnurMustafayev@users.noreply.github.com>
2020-11-14 20:58:55 +0400

    Add Multithread example

---
+++ Multithreading/Department.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Multithreading {
+    public class Department {
+        // all processes' actions
+        public List<Action> actions;
+
+        // BEHAVIOR for DoStuff() method
+        public enum BEHAVIOR { AWAIT, SYNC, PARALLEL};
+        public BEHAVIOR behavior;
+
+        public Department(BEHAVIOR behavior, params Action[] actions) {
+            this.actions = new List<Action>(actions);
+            this.behavior = behavior;
+        }
+
+        // Execute all actions depending on the behavior
+        public void DoStuff() {
+            switch(this.behavior) {
+                case BEHAVIOR.AWAIT:
+                    this.DoStuffWithAwait();
+                    break;
+                case BEHAVIOR.SYNC:
+                    this.DoStuffSync();
+                    break;
+                case BEHAVIOR.PARALLEL:
+                    this.DoStuffParallel();
+                    break;
+                default:
+                    Console.WriteLine($"No realization for '{this.behavior}' behaviour!");
+                    break;
+            }
+        }
+
+        // multithread,     async, step by step
+        private async void DoStuffWithAwait() {
+            foreach(var action in actions)
+                await this.DoTaskAsync(action);
+        }
+        // singlethread,    sync, step by step
+        private void DoStuffSync() => this.actions.ForEach(a => a?.Invoke());
+        // multithread,     parallel, many threads in one time
+        private void DoStuffParallel() => this.actions.ForEach(a => this.DoTaskAsync(a));
+
+        // method for "awaiting" actions from the outside
+        protected Task DoTaskAsync(Action action) => Task.Run(action);
+    }
+}
---
+++ Multithreading/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Multithreading {
+    class Program {
+        // process item
+        class TaskList {
+            // will be filled during the process
+            public List<int> List = new List<int>();
+            // number of taсеs
+            public int Final;
+
+            public TaskList(int final) => this.Final = final;
+        }
+
+        // each task -> each process and progress bar in console
+        static TaskList[] tasks = {
+                new TaskList(50),
+                new TaskList(38),
+                new TaskList(44),
+                new TaskList(23),
+                new TaskList(13),
+            };
+
+        static void Main() {
+            // set behaviour (parallel, async, ...)
+            var behavior = Department.BEHAVIOR.PARALLEL;
+
+            // for optimization:
+            // if behavior is parallel -> only the first action will print all progress bars
+            Action toprint = null;
+            if(behavior != Department.BEHAVIOR.PARALLEL)
+                toprint = () => PrintLists();
+
+            // actions which will be called depending on the behavior
+            Action[] actions = new Action[] {
+                // init actions
+                () => AddNumbers(tasks[0], PrintLists),
+                () => AddNumbers(tasks[1], toprint),
+                () => AddNumbers(tasks[2], toprint, 150),
+                () => AddNumbers(tasks[3], toprint),
+                () => AddNumbers(tasks[4], toprint, 400),
+            };
+
+            #region DOESN'T WORK !!! TOFIX
+            //actions.Add(() => AddNumbers(tasks[0], PrintLists));
+            //for(int i = 1; i < tasks.Length; i++)
+            //    actions.Add(() => AddNumbers(tasks[i], toprint));
+            #endregion
+
+            // create department and throw actions there
+            Department department = new Department(behavior, actions);
+
+            // execute all actions
+            department.DoStuff();
+
+            // bcz parallel execution
+            Console.ReadKey();
+        }
+
+        // the main method of each process
+        static void AddNumbers(TaskList task, Action action = null, int milliseconds = 100) {
+            for(int i = 0; i < task.Final; i++) {
+                // some stuff... (nothing serious)
+                task.List.Add(i);
+
+                // calling an additional method during the main process if it exists
+                action?.Invoke();
+
+                // slowing down the process
+                Thread.Sleep(milliseconds);
+            }
+        }
+
+        // prints all processes
+        static void PrintLists() {
+            Console.Clear();
+            for(int i = 0; i < tasks.Length; i++)
+                PrintList(tasks[i], $"{i + 1} Thread: \t");
+        }
+
+        // prints each processes
+        static void PrintList(TaskList task, string message = "") {
+            Console.Write(message);
+            int percent = (100 * task.List.Count) / task.Final;
+            PrintProgressBar(percent);
+            Console.WriteLine();
+        }
+
+        // prints progress bar for given process
+        static void PrintProgressBar(int percent, int barSize = 4) {
+            Console.ForegroundColor = percent == 100 ? ConsoleColor.Green : ConsoleColor.Yellow;
+            Console.Write($"{percent} % \t");
+
+            // progress bar
+            percent /= barSize;
+            Console.Write('[');
+            for(int i = 0; i < (100 / barSize); i++)
+                Console.Write(i < percent ? '|' : '.');
+            Console.Write(']');
+
+            Console.ResetColor();
+        }
+    }
+}
